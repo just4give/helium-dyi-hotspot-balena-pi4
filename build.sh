10ff2ababad54f3e99f9f1712340615eea9300d4
Kovuru Sai Charan <saicharank@hotmail.com>
2020-07-23 23:28:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 cp docker-compose-template.yml docker-compose.yml 
 export MINER_TAG=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=100&page=1&onlyActiveTags=true' | jq -c '[ .tags[] | select( .name | contains("arm")) ][0].name' | cut -d'"' -f2)
-sed -i '' "s/MINER_TAG/$MINER_TAG/g" docker-compose.yml
+sed.exe s/MINER_TAG/$MINER_TAG/g docker-compose.yml > docker-temp.yml
+cp docker-temp.yml docker-compose.yml
