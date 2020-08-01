005858d4eabacbf86eb7722ee13c5130815ba38c
Kovuru Sai Charan <saicharank@hotmail.com>
2020-08-02 01:28:48 +0530

    Update start.sh

--- pigateway/start.sh
+++ pigateway/start.sh
@@ -14,16 +14,17 @@ python3 run.py
 
 # Reset RAK2245 PIN
 SX1301_RESET_BCM_PIN=17
-echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
-echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+echo "$SX1301_RESET_BCM_PIN"  |sudo tee /sys/class/gpio/export
+echo "out" |sudo tee /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
+echo "0"   |sudo tee /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
-echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+echo "1"   |sudo tee /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
-echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+echo "0"   |sudo tee /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
-echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
+echo "$SX1301_RESET_BCM_PIN" |sudo tee /sys/class/gpio/unexport
 sleep 0.2
 
 cd /build/packet_forwarder/lora_pkt_fwd
 ./lora_pkt_fwd
+
