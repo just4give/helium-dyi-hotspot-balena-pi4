56fd3da7426f3bfbece531e9e9fcddf30d160429
Mithun Das <mithun.das@coxautoinc.com>
2020-08-04 08:57:49 -0400

    modifted sed command

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 cp docker-compose-template.yml docker-compose.yml 
 export MINER_TAG=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=100&page=1&onlyActiveTags=true' | jq -c '[ .tags[] | select( .name | contains("arm")) ][0].name' | cut -d'"' -f2)
-sed -i '' "s/MINER_TAG/$MINER_TAG/g" docker-compose.yml
+sed -i -e "s/MINER_TAG/$MINER_TAG/g" docker-compose.yml
