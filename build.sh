b30d4954fa7049e6d8d594385b1d1dec4669ac84
Chimi59 <115189355+Chimi59@users.noreply.github.com>
2022-10-06 17:09:22 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
     cp docker-compose-template.yml docker-compose.yml 
     export MINER_TAG=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=100&page=1&onlyActiveTags=true' | jq -c '[ .tags[] | select( .namecontains("arm")) ][0].name' | cut -d'"' -f2)
     sed.exe s/MINER_TAG/$MINER_TAG/g docker-compose.yml > docker-temp.yml
-    cp docker-temp.yml docker-compose.yml
+   
